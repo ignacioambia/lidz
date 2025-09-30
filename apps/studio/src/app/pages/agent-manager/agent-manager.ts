75a3d5b8b9d36482cd951745c033323b530fcca9
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-09-30 14:39:10 -0600

    feat(agent-manager): enhance agent manager UI with action cards and instructions handling

--- apps/studio/src/app/pages/agent-manager/agent-manager.ts
+++ apps/studio/src/app/pages/agent-manager/agent-manager.ts
@@ -1,42 +1,91 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { Location } from '@angular/common';
 import { ChatSandbox } from './chat-sandbox/chat-sandbox';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { AgentsAPI } from '@lidz/shared';
+import { AgentAction, AgentsAPI } from '@lidz/shared';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AgentsService } from '../../services/agents-service';
-import { retry } from 'rxjs';
+import { ActionCard } from './action-card/action-card';
+import { Button } from '../../button/button';
+import { LucideAngularModule, ZapOffIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-agent-manager',
-  imports: [ChatSandbox, FormsModule],
+  imports: [ChatSandbox, FormsModule, ActionCard, Button, LucideAngularModule],
   templateUrl: './agent-manager.html',
   styleUrl: './agent-manager.scss',
 })
 export class AgentManager {
+  readonly ZapOffIcon = ZapOffIcon
   public instructions = signal<string>('');
+  private actions = signal<AgentAction[]>([
+    {
+      type: 'NOTIFICATION',
+      status: 'rejected',
+      tool: {
+        name: 'notificar_seleccion_de_fruta',
+        description:
+          'Notifica cuando el usuario haya seleccionado una fruta de la lista proporcionada.',
+        parameters: {
+          primer_tiempo: 'Nombre de la fruta seleccionada por el usuario.',
+          segundo_tiempo:
+            'Nombre de la otra fruta seleccionada por el usuario.',
+          tercer_tiempo: 'Nombre de la otra fruta seleccionada por el usuario.',
+        },
+      },
+    },
+    // {
+    // type: "NOTIFICATION",
+    // status: "confirmed",
+    // tool: {
+    //   name: "notificar_seleccion_de_fruta",
+    //   description: "Notifica cuando el usuario haya seleccionado una fruta de la lista proporcionada.",
+    //   parameters: {
+    //       fruta_seleccionada: "Nombre de la fruta seleccionada por el usuario."
+    //     }
+    //   }
+    // },
+    // {
+    // type: "NOTIFICATION",
+    // status: "rejected",
+    // tool: {
+    //   name: "notificar_seleccion_de_fruta",
+    //   description: "Notifica cuando el usuario haya seleccionado una fruta de la lista proporcionada.",
+    //   parameters: {
+    //       fruta_seleccionada: "Nombre de la fruta seleccionada por el usuario."
+    //     }
+    //   }
+    // }
+  ]);
+
+  public displayedActions = computed(() =>
+    this.showRejected()
+      ? this.actions()
+      : this.actions().filter((action) => action.status !== 'rejected')
+  );
   private location = inject(Location);
   private agentsService = inject(AgentsService);
   private route = inject(ActivatedRoute);
 
   public agentId = signal<string>('');
+  public showRejected = signal<boolean>(false);
 
   private agentIdEffect = effect(() => {
     const agentId = this.agentId();
     if (agentId) {
       // Fetch agent details using the agentId
 
-        this.agentsService.getById(agentId).subscribe({
-          next: (response) => {
-            this.instructions.set(response.instructions);
-            console.log('Just received instructions', this.instructions() );
-          },
-          error: (err) => {
-            console.error('Error fetching agent details', err);
-          },
-        });
-    } else {    
+      this.agentsService.getById(agentId).subscribe({
+        next: (response) => {
+          this.instructions.set(response.instructions);
+          console.log('Just received instructions', this.instructions());
+        },
+        error: (err) => {
+          console.error('Error fetching agent details', err);
+        },
+      });
+    } else {
       this.instructions.set(''); // Reset instructions if no agentId
     }
   });
@@ -66,5 +115,10 @@ export class AgentManager {
           console.error('Error updating instructions ', err);
         },
       });
+  }
+
+  public onShowRejectedChange(event: Event) {
+    console.log('Calling setting rejected');
+    this.showRejected.set((event.target as HTMLInputElement).checked);
   }
 }
