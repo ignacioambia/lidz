be8008504f4f88a252d485e0713795ff54c9693b
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-10-06 21:13:36 -0600

    feat(mobile-app): Add initial iOS app configuration and launch screen
    - Created LaunchScreen.storyboard for the app's launch screen with a splash image.
    - Added Main.storyboard for the main app interface, utilizing a bridge view controller.
    - Configured Info.plist with essential app metadata, including display name and storyboard references.
    - Established Podfile for managing dependencies, specifically for Capacitor and CapacitorCordova.
    - Generated Podfile.lock to lock the versions of the installed pods.

---
+++ apps/mobile-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lidz.ignites.me',
+  appName: 'Lidz: Instant Answers, Seamless Updates',
+  webDir: 'dist/mobile-app/browser'
+};
+
+export default config;
