652d40733bd220f013860691a17d1d9cadfdc4cb
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-10-12 18:03:49 -0600

    feat(routes): add routing configuration for home, chats, actions, settings, and conversation components feat(index): update viewport meta tag for better mobile responsiveness

--- apps/mobile-app/src/app/app.routes.ts
+++ apps/mobile-app/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+        children: [
+            { path: '', redirectTo: 'actions', pathMatch: 'full' },
+            {
+                path: 'chats',
+                loadComponent: () => import('./pages/home/chats/chats').then(m => m.Chats)
+            },
+            {
+                path: 'actions',
+                loadComponent: () => import('./pages/home/actions/actions').then(m => m.Actions)
+            },
+            {
+                path: 'settings',
+                loadComponent: () => import('./pages/home/settings/settings').then(m => m.Settings)
+            },
+        ]
+    },
+    {
+        path: 'conversation/:conversationId',
+        loadComponent: () => import('./pages/conversation/conversation').then(m => m.Conversation)
+    }
+];
