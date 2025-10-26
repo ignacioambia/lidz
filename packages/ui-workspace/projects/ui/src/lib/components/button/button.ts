775796a8697910a91e415aaf44ae7036fc13d586
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-10-26 16:06:28 -0600

    feat(button): integrate LoaderDots component and enhance loading state handling

--- packages/ui-workspace/projects/ui/src/lib/components/button/button.ts
+++ packages/ui-workspace/projects/ui/src/lib/components/button/button.ts
@@ -1,14 +1,21 @@
 import { Component, HostBinding, input } from '@angular/core';
+import { LdLoaderDots } from '../loader-dots/loader-dots';
 
 @Component({
   selector: 'button[lidz-button]',
-  imports: [],
+  imports: [LdLoaderDots],
   templateUrl: './button.html',
-  styleUrl: './button.scss'
+  styleUrl: './button.scss',
+  host: {
+    '[class.disabled]': 'disabled()',
+    '[class.loading]': 'loading()',
+  },
 })
 export class Button {
   variant = input<'outline' | 'solid'>('solid');
   type = input<'confirm' | 'cancel'>('confirm');
+  disabled = input<boolean>(false);
+  loading = input<boolean>(false);
 
   @HostBinding('attr.data-variant')
   get variantAttr() {
