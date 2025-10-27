3f89e4f92909f3ea0e31a73fc3f736b738897084
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-10-26 20:12:14 -0600

    feat(login): enhance login flow with input validation and loading states

--- apps/mobile-app/src/app/pages/login/login.ts
+++ apps/mobile-app/src/app/pages/login/login.ts
@@ -1,42 +1,54 @@
 import { Component, inject, signal } from '@angular/core';
 import { Button, LidzInput } from '@lidz/ui';
-import { FormsModule, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { PHONE_NUMBER_REGEX, VERIFICATION_CODE_REGEX } from '@lidz/shared';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../services/auth-service';
 import { Clipboard } from '@capacitor/clipboard';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    Button,
-    LidzInput,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [Button, LidzInput, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  public phoneNumberControl = new FormControl('', [Validators.required]);
-  public verificationCode = new FormControl('', [Validators.required]);
+  public phoneNumberControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(PHONE_NUMBER_REGEX),
+  ]);
+  public verificationCode = new FormControl('', [
+    Validators.required,
+    Validators.pattern(VERIFICATION_CODE_REGEX),
+  ]);
   currentStep = signal<number>(0);
   private authService = inject(AuthService);
+
+  public sendingCode = false;
 
   verifyNumber() {
     const phoneNumber = this.phoneNumberControl.value;
 
-    if(!phoneNumber) {
+    if (!phoneNumber) {
       console.error('Phone number is required');
       return;
     }
-    
+    this.sendingCode = true;
     this.authService.sendWaCode(phoneNumber).subscribe({
       next: (response) => {
         console.log('WA code sent successfully', response);
         this.currentStep.set(1);
+        this.sendingCode = false;
       },
       error: (error) => {
         console.error('Error sending WA code', error);
-      }
+        this.sendingCode = false;
+      },
     });
   }
 
@@ -50,5 +62,4 @@ export class Login {
       console.error('Error reading from clipboard', error);
     }
   }
-
 }
